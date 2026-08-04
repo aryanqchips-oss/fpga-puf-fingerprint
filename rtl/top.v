0271d9ba9b34d186caffa7296f8ed3234a6e26d0
aryanqchips-oss <aryan.qchips@gmail.com>
2026-08-04 22:48:08 +0530

    Update top.v

--- rtl/top.v
+++ rtl/top.v
@@ -1,1 +1,44 @@
+module top(
 
+    input wire clk,
+    input wire reset,
+
+    output wire response
+
+);
+
+wire osc;
+
+wire [31:0] count;
+
+ring_oscillator ro(
+
+    .enable(1'b1),
+
+    .osc_out(osc)
+
+);
+
+counter counter0(
+
+    .clk(clk),
+
+    .reset(reset),
+
+    .enable(1'b1),
+
+    .count(count)
+
+);
+
+comparator cmp(
+
+    .countA(count),
+
+    .countB(32'd100),
+
+    .response(response)
+
+);
+
+endmodule
